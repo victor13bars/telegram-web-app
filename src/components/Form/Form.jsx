732867b6ab34b5d589e91c05cca75bc028d3e506
victor13bars <victor_bars_bars95@mail.ru>
2022-10-24 16:57:19 +0300

    small refactor.

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -18,11 +18,11 @@ const Form = () => {
     }, [country, street, subject])
 
     useEffect(() => {
-        tg.WebApp.onEvent('mainButtonClicked', onSendData)
+        tg.onEvent('mainButtonClicked', onSendData)
         return () => {
-            tg.WebApp.offEvent('mainButtonClicked', onSendData)
+            tg.offEvent('mainButtonClicked', onSendData)
         }
-    }, [])
+    }, [onSendData()])
 
     useEffect(() => {
         tg.MainButton.setParams({
